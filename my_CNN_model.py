e760ecc8a1275b58060b073b9523d3bdbe336acb
cavidparker <cavidpark@gmail.com>
2020-04-29 16:56:36 +0600

    2nd commit

--- my_CNN_model.py
+++ my_CNN_model.py
@@ -1,34 +1,41 @@
-def get_my_CNN_model_arch():
-	model = Sequential()
-	model.add(Convolution2D(32, (5, 5), input_shape=(96,96,1), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
 
-	model.add(Convolution2D(64, (3, 3), activation = 'relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.1))
+def get_my_CNN_model_architecture():
+  
+    model = Sequential()
+    model.add(Convolution2D(32, (5, 5), input_shape=(96,96,1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-	model.add(Convolution2D(128, (3, 3), activation= 'relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.2))
+    model.add(Convolution2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.1))
 
-	model.add(Convolution2D(30, (3, 3), activation='relu'))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	model.add(Dropout(0.3))
+    model.add(Convolution2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-	model.add(Flatten())
+    model.add(Convolution2D(30, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.3))
 
-	model.add(Dense(64, activation='relu'))
-	model.add(Dense(128, activation='relu'))
-	model.add(Dense(256, activation='relu'))
-	model.add(Dense(64, activation='relu'))
-	model.add(Dense(30))
+    model.add(Flatten())
 
-	return model;
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(30))
 
-def compile_my_cnn_model(model, optimizer, loss, metrics):
-	model.compile(optimizer=optimizer,loss=loss, metrics=metrics)
+    return model;
+
+
+def compile_my_CNN_model(model, optimizer, loss, metrics):
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
 def train_my_CNN_model(model, X_train, y_train):
-	return model.fit(X_train, y_train, epochs=100, batch_size=200, verbose=1, validation_split = 0.2
-		
+    return model.fit(X_train, y_train, epochs=100, batch_size=200, verbose=1, validation_split=0.2)
 
+def save_my_CNN_model(model, fileName):
+    model.save(fileName + '.h5')
+
+def load_my_CNN_model(fileName):
+    return load_model(fileName + '.h5')
